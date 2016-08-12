aee3ea867c401aee5e42e9a55467f7fc617f19e0
Aneta <wozniakaneta5@gmail.com>
2016-08-12 22:18:33 +0200

    paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -19,13 +19,6 @@ ActiveRecord::Schema.define(version: 20160812120557) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "blog_articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
     t.text     "body"
