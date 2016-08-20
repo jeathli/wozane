8610823931fe48f4cc5b50bf0af18723d2b92e25
Wozane <wozniakaneta5@gmail.com>
2016-08-20 12:54:33 +0000

    wip

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,8 @@
 //= require bootstrap-sprockets
 //= require bootstrap
 //= require masonry/jquery.masonry
+//= require masonry/jquery.event-drag
+//= require masonry/jquery.imagesloaded.min
+//= require masonry/jquery.infinitescroll.min
+//= require masonry/modernizr-transitions
 //= require_tree .
