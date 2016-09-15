a1d13173c44ee16b3f0775053c0afd281f784945
Wozane <wozniakaneta5@gmail.com>
2016-09-15 08:23:37 +0000

    update of aritcle model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   validates :title, presence: true,
-            length: { minimum: 5 },
+            length: { minimum: 5 }
   validates :img_url, presence: true
 end
