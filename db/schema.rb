f7e0061948c72271bfc7a95b33934aa8206561a3
Aneta Wozniak <wozniakaneta5@gmail.com>
2016-09-21 12:29:46 +0200

    Rspec (#30)
    * rspec for articles_controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160917192940) do
+ActiveRecord::Schema.define(version: 20160919081009) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
--- spec/controllers/blog/articles_controller_spec.rb
+++ spec/controllers/blog/articles_controller_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Blog::ArticlesController, type: :controller do
-  let(:article) { FactoryGirl.create(:article) }
-
   describe "GET #index" do
-    let!(:footest_article) { FactoryGirl.create(:article, title: "footest") }
-    let!(:bartest_article) { FactoryGirl.create(:article, title: "bartest") }
+    let!(:older_article) { FactoryGirl.create(:article, created_at: 1.hour.ago) }
+    let!(:newer_article) { FactoryGirl.create(:article) }
 
     before { get :index }
 
@@ -13,32 +11,136 @@ RSpec.describe Blog::ArticlesController, type: :controller do
       expect(response).to have_http_status(:ok)
     end
 
+    it "renders the index template" do
+      expect(response).to render_template("index")
+    end
+
     it "returns sortered results" do
-      expect(assigns(:articles)).to eq([bartest_article, footest_article])
+      expect(assigns(:articles)).to eq([newer_article, older_article])
     end
   end
 
   describe "GET #show" do
+    let!(:article) { FactoryGirl.create(:article) }
+
     before do
-      get :show, params: {id: article.to_param}
+      get :show, params: {id: article.id}
     end
 
-    it "assigns the requested article as @article" do
-      expect(assigns(:article)).to eq(article)
+    it "returns 200 OK" do
+      expect(response).to have_http_status(:ok)
     end
 
     it "renders the show template" do
       expect(response).to render_template("show")
     end
+
+    it "assigns the requested article as @article" do
+      expect(assigns(:article)).to eq(article)
+    end
   end
 
   describe "GET #new" do
-    before do
-      get :new
+    context "unlogged user" do
+      before do
+        get :new
+      end
+
+      it "redirects to login page" do
+        expect(response).to redirect_to(login_path)
+      end
     end
 
-    it "renders the new template" do
-      expect(response).to render_template("new")
+    context "logged user" do
+      before do
+        session[:admin] = true
+        get :new
+      end
+
+      it "returns 200 OK" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "assigns the requested as @article" do
+        expect(assigns(:article)).to be_a_new(Article)
+      end
+
+      it "renders the new template" do
+        expect(response).to render_template("new")
+      end
+    end
+  end
+
+  describe "POST #create" do
+    context "unlogged user" do
+      before do
+        post :create
+      end
+
+      it "redirects to login page" do
+        expect(response).to redirect_to(login_path)
+      end
+    end
+
+    context "valid params" do
+      before do
+        session[:admin] = true
+        post :create, params: { article: {
+          title: "Testbar",
+          text: "feel the love",
+          img_url: 'http://example.com/image.jpg'
+        } }
+      end
+
+      it "redirects to created_article" do
+        expect(response).to redirect_to(blog_articles_path)
+      end
+    end
+
+    context "invalid params" do
+      before do
+        session[:admin] = true
+        post :create, params: { article: {
+          title: "Foo",
+          text: "feel the love",
+          img_url: 'http://example.com/image.jpg'
+        } }
+      end
+
+      it "re-renders the 'new' template" do
+        expect(response).to render_template(:new)
+      end
+
+      it "raises error" do
+        expect { Article.new.foo }.to raise_error
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    let(:article) { FactoryGirl.create(:article) }
+
+    context "valid params" do
+      let(:new_article) { {
+        title: "Testbar",
+        text: "feel the love",
+        img_url: 'http://example.com/image.jpg'
+      }}
+
+      before do
+        session[:admin] = true
+        put :update, params: { article: {
+          title: "Foobar"
+        } }
+      end
+
+      it "assigns the requested article as @article" do
+        expect(assigns(:article)).to eq(article)
+      end
+
+      xit "renders edit template" do
+        expect(response).to render_template(:edit)
+      end
     end
   end
 end
