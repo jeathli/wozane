3d881d51f3a6adf853d8a9030bc6a6552631fca9
Wozane <wozniakaneta5@gmail.com>
2016-09-26 12:43:44 +0000

    heroku db:migration fix

--- db/migrate/20160917192940_remove_column_img.rb
+++ db/migrate/20160917192940_remove_column_img.rb
@@ -1,8 +1,8 @@
 class RemoveColumnImg < ActiveRecord::Migration[5.0]
   def change
-    remove_column :articles, :img_url_file_name , :string
-    remove_column :articles, :img_url_content_type, :string
-    remove_column :articles, :img_url_file_size, :integer
-    remove_column :articles, :img_url_updated_at, :datetime
+    #remove_column :articles, :img_url_file_name , :string
+    #remove_column :articles, :img_url_content_type, :string
+    #remove_column :articles, :img_url_file_size, :integer
+    #remove_column :articles, :img_url_updated_at, :datetime
   end
 end
